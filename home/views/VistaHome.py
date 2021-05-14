40a2b96cc26256031789fdc6f13aa5f931dff84a
TommasoRiva <s1095496@studenti.univpm.it>
2021-05-14 11:22:26 +0200

    prima commit

---
+++ home/views/VistaHome.py
@@ -0,0 +1,46 @@
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+
+#from listaclienti.views.VistaListaClienti import VistaListaClienti
+#from listadipendenti.views.VistaListaDipendenti import VistaListaDipendenti
+#from listaprenotazioni.views.VistaListaPrenotazioni import VistaListaPrenotazioni
+#from listaservizi.views.VistaListaServizi import VistaListaServizi
+
+
+class VistaHome(QWidget):
+
+    def __init__(self, parent=None):
+        super(VistaHome, self).__init__(parent)
+        grid_layout = QGridLayout()
+
+        grid_layout.addWidget(self.get_generic_button("Prodotti", self.go_lista_servizi), 0, 0)
+        grid_layout.addWidget(self.get_generic_button("Lista Clienti", self.go_lista_clienti), 0, 1)
+        grid_layout.addWidget(self.get_generic_button("Carrello", self.go_lista_dipendenti), 1, 0)
+
+        self.setLayout(grid_layout)
+        self.resize(400, 300)
+        self.setWindowTitle("Gestore Stabilimento PRO")
+
+    '''
+    Questa funzione restituisce un bottone generico dato il titolo
+    '''
+    def get_generic_button(self, titolo, on_click):
+        button = QPushButton(titolo)
+        button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        button.clicked.connect(on_click)
+        return button
+
+    def go_lista_servizi(self):
+        self.vista_lista_servizi = VistaListaServizi()
+        self.vista_lista_servizi.show()
+
+    def go_lista_clienti(self):
+        self.vista_lista_clienti = VistaListaClienti()
+        self.vista_lista_clienti.show()
+
+    def go_lista_dipendenti(self):
+        self.vista_lista_dipendenti = VistaListaDipendenti()
+        self.vista_lista_dipendenti.show()
+
+    def go_lista_prenotazioni(self):
+        self.vista_lista_prenotazioni = VistaListaPrenotazioni()
+        self.vista_lista_prenotazioni.show()
