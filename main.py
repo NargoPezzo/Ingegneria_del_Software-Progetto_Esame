f67e5444921fa85838c1b363aaf432db2e48b4ba
TommasoRiva <s1095496@studenti.univpm.it>
2021-05-18 12:32:41 +0200

    login wip

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from home.views.VistaHome import VistaHome
+from login.views.VistaLogin import VistaLogin
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    vista_login = VistaLogin()
+    vista_login.show()
     vista_home = VistaHome()
     vista_home.show()
+
     sys.exit(app.exec())
