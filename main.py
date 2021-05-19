20b534d805b77c91d128cabd0c4959cf83bd426b
TommasoRiva <s1095496@studenti.univpm.it>
2021-05-19 17:07:53 +0200

    login working

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from PyQt5.QtWidgets import QApplication
 from login.views.VistaLogin import VistaLogin
 
 
-
-app = QApplication(sys.argv)
-main = VistaLogin()
-main.show()
-
-sys.exit(app.exec())
-
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    main = VistaLogin()
+    main.show()
+
+    sys.exit(app.exec())
+
