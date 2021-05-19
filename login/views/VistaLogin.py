d06a76ab5baaab79a1cd8a784ba7d26dad9936af
TommasoRiva <s1095496@studenti.univpm.it>
2021-05-19 16:48:30 +0200

    login working

--- login/views/VistaLogin.py
+++ login/views/VistaLogin.py
@@ -8,20 +8,20 @@ from home.views.VistaHome import VistaHome
 class VistaLogin(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Login Form')
+        self.setWindowTitle('Login')
         self.resize(400, 120)
 
         layout = QGridLayout()
 
         label_name = QLabel('<font size="4"> Username </font>')
         self.lineEdit_username = QLineEdit()
-        self.lineEdit_username.setPlaceholderText('Please enter your username')
+        self.lineEdit_username.setPlaceholderText('Inserire nome Dipendente')
         layout.addWidget(label_name, 0, 0)
         layout.addWidget(self.lineEdit_username, 0, 1)
 
         label_password = QLabel('<font size="4"> Password </font>')
         self.lineEdit_password = QLineEdit()
-        self.lineEdit_password.setPlaceholderText('Please enter your password')
+        self.lineEdit_password.setPlaceholderText('Inserire password')
         layout.addWidget(label_password, 1, 0)
         layout.addWidget(self.lineEdit_password, 1, 1)
 
@@ -42,6 +42,7 @@ class VistaLogin(QWidget):
             msg.setText('Benvenuto')
             msg.exec_()
             self.dialog.show()
+
 
 
         else:
