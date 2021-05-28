1e5532aec5a835f7f0fbfc9c70e7c0d3d5e0afb6
Alex Pugnaloni <s1092591@studenti.univpm.it>
2021-05-28 11:01:29 +0200

    quasi fix vista home dipendente

--- home/views/VistaHome.py
+++ home/views/VistaHome.py
@@ -1,25 +1,33 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy, QLabel
 
 from carrello.views.VistaCarrello import VistaCarrello
 from listaclienti.views.VistaListaClienti import VistaListaClienti
 from listaprodotti.views.VistaListaProdotti import VistaListaProdotti
 from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
 
 
 class VistaHome(QWidget):
 
     def __init__(self, parent=None):
         super(VistaHome, self).__init__(parent)
+
         grid_layout = QGridLayout()
 
-        grid_layout.addWidget(self.get_generic_button("Lista Prodotti", self.go_lista_prodotti), 0, 0)
-        grid_layout.addWidget(self.get_generic_button("Lista Clienti", self.go_lista_clienti), 0, 1)
-        grid_layout.addWidget(self.get_generic_button("Carrello", self.go_carrello), 1, 0)
+        grid_layout.addWidget(self.get_icon('logos/home logos/magazzino.png'), 0, 0)
+        grid_layout.addWidget(self.get_icon('logos/home logos/clienti.png'), 0, 1)
+        grid_layout.addWidget(self.get_icon('logos/home logos/carrello.png'), 0, 2)
+
+        grid_layout.addWidget(self.get_generic_button("Lista Prodotti", self.go_lista_prodotti), 1, 0)
+        grid_layout.addWidget(self.get_generic_button("Lista Clienti", self.go_lista_clienti), 1, 1)
+        grid_layout.addWidget(self.get_generic_button("Carrello", self.go_carrello), 1, 2)
 
         self.setLayout(grid_layout)
-        self.resize(400, 300)
+        self.setFixedSize(700, 200)
         self.setWindowTitle("Negozio di Elettronica")
         self.setWindowIcon(QtGui.QIcon('logos/logo.png'))
+
 
     '''
     Questa funzione restituisce un bottone generico dato il titolo
@@ -29,6 +37,16 @@ class VistaHome(QWidget):
         button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         button.clicked.connect(on_click)
         return button
+
+    def get_icon(self, path):
+        label_logo = QLabel(self)
+        pixmap = QPixmap(path)
+        pixmap = pixmap.scaledToWidth(120)
+        pixmap = pixmap.scaledToHeight(120)
+        label_logo.setPixmap(pixmap)
+        label_logo.setFixedSize(120, 120)
+        label_logo.setAlignment(Qt.AlignCenter)
+        return label_logo
 
     def go_lista_prodotti(self):
         self.vista_lista_prodotti = VistaListaProdotti()
